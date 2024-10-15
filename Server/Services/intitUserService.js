8085a7d23471b0f64de25da812ff6da46bc7346d
Vishnu Gsk <vishnugsk@Vishnus-MacBook-Air.local>
2024-10-15 20:34:06 +0530

    cahnges for abyenkar

--- Server/Services/intitUserService.js
+++ Server/Services/intitUserService.js
@@ -1,3 +1,46 @@
+/*
+ * Trinom Digital Pvt Ltd ("COMPANY") CONFIDENTIAL                             *
+ * Copyright (c) 2024 Trinom Digital Pvt Ltd, All rights reserved              *
+ *                                                                             *
+ * NOTICE:  All information contained herein is, and remains the property      *
+ * of COMPANY. The intellectual and technical concepts contained herein are    *
+ * proprietary to COMPANY and may be covered by Indian and Foreign Patents,    *
+ * patents in process, and are protected by trade secret or copyright law.     *
+ * Dissemination of this information or reproduction of this material is       *
+ * strictly forbidden unless prior written permission is obtained from         *
+ * COMPANY. Access to the source code contained herein is hereby forbidden     *
+ * to anyone except current COMPANY employees, managers or contractors who     *
+ * have executed Confidentiality and Non-disclosure agreements explicitly      *
+ * covering such access.                                                       *
+ *                                                                             *
+ * The copyright notice above does not evidence any actual or intended         *
+ * publication or disclosure of this source code, which includes               *
+ * information that is confidential and/or proprietary, and is a trade secret, *
+ * of COMPANY. ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC            *
+ * PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE        *
+ * WITHOUT THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,      *
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE         *
+ * RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES   *
+ * NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS     *
+ * CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE,    *
+ * IN WHOLE OR IN PART.                                                        *
+ *                                                                             *
+ * File: /Services/intitUserService.js                                         *
+ * Project: Server                                                             *
+ * Created Date: Tuesday, October 15th 2024, 8:31:05 pm                        *
+ * Author: Vishnu <vishnu@codestax.ai>                                         *
+ * -----                                                                       *
+ * Last Modified:                                                              *
+ * Modified By:                                                                *
+ * -----                                                                       *
+ * Any app that can be written in JavaScript,                                  *
+ *     will eventually be written in JavaScript !!                             *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date         By  Comments                                                   *
+ * --------------------------------------------------------------------------- *
+ */
+
 const db = require('./dbService')
 const InitUserService = ((req,res)=>{
     console.log("Init User Service  invoked");
@@ -7,7 +50,7 @@ const InitUserService = ((req,res)=>{
     (error,results)=>{
         if (error) throw error;
         // If the account exists
-        console.log(results);
+        console.log('resulttttt by abaynkar',results);
         
         res.end();
             }
